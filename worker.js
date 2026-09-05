238b1032ed10ade1e393c09649c6521c14ea610b
raincandy0603-ops <raincandy0603@gmail.com>
2026-09-05 11:36:28 +0900

    Add files via upload

---
+++ worker.js
@@ -0,0 +1,6 @@
+import { WebWorkerMLCEngineHandler } from "https://esm.run/@mlc-ai/web-llm@0.2.84";
+
+const handler = new WebWorkerMLCEngineHandler();
+self.onmessage = (event) => {
+  handler.onmessage(event);
+};
